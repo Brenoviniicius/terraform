bc4c7b88dd1c6f1d98abad1cb028b4f5b5d50645
Breno Vínicius <brenovmsilva82@gmail.com>
2024-10-15 21:04:55 -0300

    Terraform plan

--- Terraform-inicio/main.tf
+++ Terraform-inicio/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  #Declarando a versão necessária para executar o .tf
+  required_version = "1.9.7"
+  
+  #Declarando o provedor e a versão do provedor do .tf
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "5.72.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-2"
   profile = "default"

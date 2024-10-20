9979fb756a23817ead26087f0662a9d3687970a8
Breno Vínicius <brenovmsilva82@gmail.com>
2024-10-20 10:58:29 -0300

    Bootstrapping the backend using a .hcl configuration file

---
+++ Terraform-inicio/Remote State/bucket/backend.hcl
@@ -0,0 +1,4 @@
+bucket  = "tfstate-084828603840"
+key     = "backend/dev/terraform.tfstate"
+region  = "us-east-2"
+profile = "default"
--- Terraform-inicio/Remote State/bucket/main.tf
+++ Terraform-inicio/Remote State/bucket/main.tf
@@ -11,10 +11,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "tfstate-084828603840"
-    key     = "backend/dev/terraform.tfstate"
-    region  = "us-east-2"
-    profile = "default"
+  
   }
 }
 
